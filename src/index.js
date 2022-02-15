8244c80b09c7c1b2b6350f73100c21da68d05f16
Vadym Starchenko <starvadim@ukr.net>
2022-02-15 15:37:45 +0200

    Initial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './sass/main.scss';
+import menu from './menu.json';
+import menuItem from './templates/menu-item.hbs';
+
+const listRef = document.querySelector('.js-menu');
+
+// const markup = menu.map(item => menuItem(item)).join('');
+const markup = menuItem(menu);
+
+console.log(markup);
+
+listRef.insertAdjacentHTML('afterbegin', markup);
