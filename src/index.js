d45d79c1ffb4943beecd7b20e6d050f8994830f4
Vadym Starchenko <starvadim@ukr.net>
2022-02-16 15:02:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,42 @@ const listRef = document.querySelector('.js-menu');
 // const markup = menu.map(item => menuItem(item)).join('');
 const markup = menuItem(menu);
 
-console.log(markup);
+// console.log(markup);
 
 listRef.insertAdjacentHTML('afterbegin', markup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const toggleSwitch = document.querySelector('#theme-switch-toggle');
+const theme = localStorage.getItem('Theme');
+
+if (!theme) {
+    document.body.classList.add(Theme.LIGHT);
+} else {
+    document.body.classList.add(theme);
+    if (theme === Theme.DARK) {
+        toggleSwitch.checked = true;
+    }
+}
+
+toggleSwitch.addEventListener('change', onChange);
+
+function onChange(e) {
+    if (e.target.checked) {
+        // document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+        // localStorage.setItem('Theme', Theme.DARK);
+        changeThemeAndSave(Theme.LIGHT, Theme.DARK);
+        return
+    }
+        changeThemeAndSave(Theme.DARK, Theme.LIGHT);
+}
+
+
+function changeThemeAndSave(themeToRemove, themeToSet) {
+    document.body.classList.replace(themeToRemove, themeToSet);
+    localStorage.setItem('Theme', themeToSet);
+}
+
